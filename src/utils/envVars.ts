b4dc3220832c9fb04954cfaae9835eb53e9b828b
Tim Jester-Pfadt <t.jp@gmx.de>
2021-12-07 15:57:53 +0100

    Initial support for devnet3

--- src/utils/envVars.ts
+++ src/utils/envVars.ts
@@ -10,9 +10,9 @@ export const INFURA_URL                 = `https://${IS_MAINNET ? "mainnet" : "g
 // public
 export const ETH2_NETWORK_NAME          = (IS_MAINNET || !process.env.REACT_APP_TESTNET_LAUNCHPAD_NAME) ? 'mainnet' : process.env.REACT_APP_TESTNET_LAUNCHPAD_NAME;
 export const TICKER_NAME                = IS_MAINNET ? 'ETH' : 'GöETH';
-export const ETHERSCAN_URL              = IS_MAINNET ? 'https://etherscan.io/tx' : 'https://goerli.etherscan.io/tx';
-export const BEACONSCAN_URL             = IS_MAINNET ? 'https://beaconscan.com/validator' : `https://beaconscan.com/${ETH2_NETWORK_NAME.toLowerCase()}/validator`;
-export const BEACONCHAIN_URL            = `https://${ETH2_NETWORK_NAME.toLowerCase()}.beaconcha.in`;
+export const ETHERSCAN_URL              = IS_MAINNET ? 'https://explorer.merge-devnet-3.wenmerge.dev/txs' : 'https://goerli.etherscan.io/tx';
+export const BEACONSCAN_URL             = IS_MAINNET ? 'https://beaconchain.merge-devnet-3.wenmerge.dev/' : `https://beaconscan.com/${ETH2_NETWORK_NAME.toLowerCase()}/validator`;
+export const BEACONCHAIN_URL            = 'https://beaconchain.merge-devnet-3.wenmerge.dev/';
 export const FORTMATIC_KEY              = process.env.REACT_APP_FORTMATIC_KEY       || 'pk_test_D113D979E0D3508F';
 export const CONTRACT_ADDRESS           = process.env.REACT_APP_CONTRACT_ADDRESS    || '0x00000000219ab540356cBB839Cbe05303d7705Fa';
 export const MIN_DEPOSIT_CLI_VERSION    = process.env.REACT_APP_MIN_DEPOSIT_CLI_VERSION  || '1.0.0';
@@ -20,8 +20,8 @@ export const LIGHTHOUSE_INSTALLATION_URL = process.env.REACT_APP_LIGHTHOUSE_INST
 export const NIMBUS_INSTALLATION_URL    = process.env.REACT_APP_NIMBUS_INSTALLATION_URL  || 'https://status-im.github.io/nimbus-eth2/intro.html';
 export const PRYSM_INSTALLATION_URL     = process.env.REACT_APP_PRYSM_INSTALLATION_URL   || 'https://docs.prylabs.network/docs/install/install-with-script';
 export const TEKU_INSTALLATION_URL      = process.env.REACT_APP_TEKU_INSTALLATION_URL    || 'https://docs.teku.pegasys.tech/en/latest/HowTo/Get-Started/Build-From-Source/';
-export const MAINNET_LAUNCHPAD_URL      = 'https://launchpad.ethereum.org/'
-export const TESTNET_LAUNCHPAD_URL      = `https://${TESTNET_LAUNCHPAD_NAME.toLowerCase()}.launchpad.ethereum.org/`
+export const MAINNET_LAUNCHPAD_URL      = 'https://storage.googleapis.com/mergedevnet-launchpad'
+export const TESTNET_LAUNCHPAD_URL      = 'https://storage.googleapis.com/mergedevnet-launchpad'
 
 if(process.env.REACT_APP_ETH_REQUIREMENT && Number.isNaN(Number(process.env.REACT_APP_ETH_REQUIREMENT))) {
     throw new Error("REACT_APP_ETH_REQUIREMENT must be of type: number")
